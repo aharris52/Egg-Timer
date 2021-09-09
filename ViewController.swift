55ce89bd3c392615c6472b7e23d4e3af77baf52b
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-09-09 16:41:09 -0700

    Linked UI label to countdown timer. Displays done on label when countdown complete.

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 300,
-                    "Medium": 420,
-                    "Hard": 720]
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft": 3,
+                    "Medium": 4,
+                    "Hard": 7]
     
     var secondsRemaining = 60
     
@@ -37,6 +38,9 @@ class ViewController: UIViewController {
         if secondsRemaining != 0 {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
         }
     }
 }
